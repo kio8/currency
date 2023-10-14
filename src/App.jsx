1ec2255cc779fb7ac9071d6f921bde883296f57d
kio8 <kioharry08@gmail.com>
2023-10-14 18:37:47 +0100

    Ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,50 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <p className="block text-black text-4xl font-bold mb-2 pt-2">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-screen h-screen bg-black  py-6">
+      <div className="w-full max-w-md rounded-lg px-4 py-3  mx-auto bg-blue-500 text-black shadow-md">
+        <h1 className="text-center font-semibold mb-1">Password Creator</h1>
+        <div className=" flex w-full  rounded-lg  text-black shadow-sm overflow-hidden ">
+          <input
+            type="text"
+            value={""}
+            placeholder="Password"
+            className="w-full rounded-md p-2 "
+          />
+          <button className="bg-black text-white text-sm px-3 outline-none py-0.5">
+            copy
+          </button>
+        </div>
+        <div className=" flex w-full   text-black text-sm gap-x-2 mt-2 overflow-hidden">
+          <input
+            type="range"
+            min={6}
+            max={20}
+            className="cursor-pointer accent-black mt-1 "
+          />
+          <label htmlFor="length"> Length :{""}</label>
+
+          <input
+            type="checkbox"
+            className="cursor-pointer accent-black mt-1"
+            name=""
+            id=""
+            defaultChecked
+          />
+          <label htmlFor="num">Numbers</label>
+
+          <input
+            type="checkbox"
+            className="cursor-pointer accent-black mt-1 "
+            name=""
+            id=""
+            defaultChecked
+          />
+          <label htmlFor="char">Characters </label>
+        </div>
+      </div>
+    </div>
   );
 }
 

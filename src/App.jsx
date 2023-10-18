427e13d05d2ba70f4f7c95ceb40b12a9ebd8a722
kio8 <kioharry08@gmail.com>
2023-10-18 08:03:00 +0100

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,59 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useCallback, useEffect, useRef } from "react";
+
 import "./App.css";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(true);
+  const [characterAllowed, setCharacterAllowed] = useState(true);
+  const [password, setPasword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed]);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "1234567890";
+    if (characterAllowed) str += "!@#$%&*_-?.";
+
+    for (let i = 1; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPasword(pass);
+  }, [length, numberAllowed, characterAllowed]);
+
+  const CopyPassword = () => {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setLength(e.target.value);
+  };
   return (
     <div className="w-screen h-screen bg-black  py-6">
       <div className="w-full max-w-md rounded-lg px-4 py-3  mx-auto bg-blue-500 text-black shadow-md">
-        <h1 className="text-center font-semibold mb-1">Password Creator</h1>
+        <h1 className="text-center font-semibold ">Password Creator</h1>
         <div className=" flex w-full  rounded-lg  text-black shadow-sm overflow-hidden ">
           <input
             type="text"
-            value={""}
+            value={password}
             placeholder="Password"
             className="w-full rounded-md p-2 "
+            readOnly
+            ref={passwordRef}
           />
-          <button className="bg-black text-white text-sm px-3 outline-none py-0.5">
+          <button
+            className="bg-black text-white text-sm px-3 outline-none py-0.5"
+            onClick={CopyPassword}
+          >
             copy
           </button>
         </div>
@@ -25,8 +63,12 @@ function App() {
             min={6}
             max={20}
             className="cursor-pointer accent-black mt-1 "
+            onChange={handleChange}
+            value={length}
+            name=""
+            id=""
           />
-          <label htmlFor="length"> Length :{""}</label>
+          <label htmlFor="length"> Length :{length}</label>
 
           <input
             type="checkbox"
@@ -34,6 +76,9 @@ function App() {
             name=""
             id=""
             defaultChecked
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
           />
           <label htmlFor="num">Numbers</label>
 
@@ -43,8 +88,11 @@ function App() {
             name=""
             id=""
             defaultChecked
+            onChange={() => {
+              setCharacterAllowed((prev) => !prev);
+            }}
           />
-          <label htmlFor="char">Characters </label>
+          <label htmlFor="charInput">Characters </label>
         </div>
       </div>
     </div>
